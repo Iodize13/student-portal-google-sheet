5f1a8f25dc459ae38dad998bab3585da85e26e4f
iodize13 <insee134@gmail.com>
2024-11-25 15:20:20 +0700

    Add register button. Fix register issue

--- pages/register.tsx
+++ pages/register.tsx
@@ -26,7 +26,7 @@ export default function Register() {
     const response = await fetch('/api/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ studentId, email, password }),
+      body: JSON.stringify({ studentId, email, password, confirmPassword }),
     });
 
     const data = await response.json();
